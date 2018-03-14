f837741c5d820b2fc71cdd8809157bec5836e89b
李铭昕 <715557344@qq.com>
2018-03-14 15:04:48 +0800

    增加基本测试

---
+++ tests/Units/MlTest.php
@@ -0,0 +1,29 @@
+<?php
+// +----------------------------------------------------------------------
+// | 基础测试类 [ WE CAN DO IT JUST THINK IT ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2016-2017 limingxinleo All rights reserved.
+// +----------------------------------------------------------------------
+// | Author: limx <715557344@qq.com> <https://github.com/limingxinleo>
+// +----------------------------------------------------------------------
+namespace Tests\Units;
+
+use Tests\UnitTestCase;
+use Phpml\Classification\KNearestNeighbors;
+
+/**
+ * Class UnitTest
+ */
+class MlTest extends UnitTestCase
+{
+    public function testBaseCase()
+    {
+        $samples = [[1, 3], [1, 4], [2, 4], [3, 1], [4, 1], [4, 2]];
+        $labels = ['a', 'a', 'a', 'b', 'b', 'b'];
+
+        $classifier = new KNearestNeighbors();
+        $classifier->train($samples, $labels);
+
+        $this->assertEquals('b', $classifier->predict([3, 2]));
+    }
+}
